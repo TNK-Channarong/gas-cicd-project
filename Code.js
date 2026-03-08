3254cae09437cd90a5994b7e8a3cff4350174275
Chnn <channarong@thanatkorn.com>
2026-03-08 21:06:25 +0700

    Update welcome message

--- Code.js
+++ Code.js
@@ -7,6 +7,8 @@ function updateSheet() {
   var ss = SpreadsheetApp.openById("18UFkvstcXdlGnnoaB1tprmP_UG5aELKuZFuJhCmd434");
   const sheet = ss.getSheetByName("CICD data");
   sheet.getRange("A1").setValue("Hello from CI/CD!");
+  sheet.getRange("B1").setValue("Deployed via CI/CD! 🚀");
+
   return "Sheet updated!";
 
 }
